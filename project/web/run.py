aa8ec33fc61e5273367106abdd36a23dff855cc5
Foozhan-Fahimzadeh <fahimzadehfoozhan@gmail.com>
2024-10-08 20:11:09 +0330

    add files

---
+++ project/web/run.py
@@ -0,0 +1,7 @@
+import subprocess
+
+def run_streamlit_app():
+    subprocess.run(["streamlit", "run", "G:\Academics\Data\DB\LAST\web\main.py"])
+
+if __name__ == "__main__":
+    run_streamlit_app()
